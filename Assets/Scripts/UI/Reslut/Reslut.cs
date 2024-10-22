0001e2696839b1b32ab66d1fdebd924be701de1e
intern.git2@compass-japan.com <interngit20>
2024-10-22 16:48:23 +0900

    結果表示を作成（動作確認はまだ）

---
+++ Assets/Scripts/UI/Reslut/Reslut.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Data;
+using Data.Database;
+using TMPro;
+public class Reslut : MonoBehaviour
+{
+    public TextMeshProUGUI weaponText;                
+    public WeaponDatabase weaponDatabase;  
+    public TextMeshProUGUI bousText;
+    public TextMeshProUGUI RankText;
+    public TextMeshProUGUI Price;
+    [SerializeField] GameObject ReslutObject;
+    public Image SowrdImage;
+
+
+    //Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    public void DisplayWeapon(Weapon weapon)
+    {
+        bousText.text += $"ボーナス: {weapon.bonus}\n";
+        Price.text += $"価格: {weapon.price}\n";
+        weaponText.text += $"レアリティ: {weapon.weapon.Rarity},長さ: { weapon.weapon.Length}," +
+            $"重さ: {weapon.weapon.Weight},鋭さ: {weapon.weapon.Sharpness}\n";
+        SowrdImage.sprite = weapon.weapon.WeaponImage;//剣の画像
+
+        // テキストを画面に表示
+
+
+        if (weapon.bonus == 0)
+        {
+
+        }
+        else if (weapon.bonus <= 5)
+        {
+
+        }
+        else if (weapon.bonus <= 10)
+        {
+
+        }
+        else if (weapon.bonus <= 15)
+        {
+
+        }
+        else if (weapon.bonus <= 20)
+        {
+            RankText.text = "SS";
+        }
+
+    }
+    //IDで捜すバージョン
+
+    //public void DisplayWeaponById(int weaponId)
+    //{
+    //    // 武器データをIDから取得
+    //    WeaponData weaponData = GetWeaponById(weaponId);
+    //    if (weaponData != null)
+    //    {
+    //        weaponText.text += $"レアリティ: {weaponData.Rarity},長さ: {weaponData.Length}," +
+    //$"重さ: {weaponData.Weight},鋭さ: {weaponData.Sharpness}\n";
+    //        SowrdImage.sprite = weaponData.WeaponImage; // 剣の画像
+
+
+    //    }
+    //    else
+    //    {
+    //        // 見つからなかった場合の処理
+    //        weaponText.text = "Weapon not found!";
+    //    }
+    //}
+
+    //private WeaponData GetWeaponById(int weaponId)
+    //{
+    //    // WeaponDatabaseからIDに基づいて武器データを取得
+    //    foreach (var weapon in weaponDatabase.GetWeaponList())
+    //    {
+    //        if (weapon.Id == weaponId)
+    //        {
+    //            return weapon;
+    //        }
+    //    }
+    //    return null; 
+    //}
+
+
+    public void BottomClose()
+    {
+        ReslutObject.SetActive(false);
+    }
+    }
+
+
